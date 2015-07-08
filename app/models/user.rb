7a06e69e0f4f137366268cbc0b9e64d40d551f46
p4x <phezco@gmail.com>
2015-07-08 15:32:10 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+
+  protected
+
+    def confirmation_required?
+      false
+    end
+
+end
