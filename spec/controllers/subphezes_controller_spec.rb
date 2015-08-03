a4ca38b903923f8995b6f8bf811558895bcc618f
Brian Davis <g_brian_davis@protonmail.com>
2015-08-02 20:58:00 -0600

    BD Atom feed shubphez show view.

--- spec/controllers/subphezes_controller_spec.rb
+++ spec/controllers/subphezes_controller_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-describe SubphezesController, 'GET #random' do
-  it 'redirects' do
+describe SubphezesController do
+  it 'redirects to random subphez for GET #random' do
     valid_pathname = /[a-zA-Z0-9]+/
     get :random
-    puts response.inspect
     expect(response).to redirect_to %r(\Ahttp://test.host/p/#{valid_pathname}\Z)
   end
 end
