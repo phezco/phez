36fec1ae1950fe42ef36702103d8af44e3c9ebdc
askl56 <amscott@openmailbox.org>
2015-08-14 20:37:52 +0100

    reorganised factories and working on users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  let(:user) { FactoryGirl.build :user }
+
+  subject { user }
+
+  it { should validate_presence_of :username }
+
+  it { should have_many(:votes) }
+  it { should have_many(:moderations) }
+  it { should have_many(:moderated_subphezes) }
+  it { should have_many(:messages) }
+  it { should have_many(:posts) }
+  it { should have_many(:comments) }
+  it { should have_many(:subphezes) }
+  it { should have_many(:subscriptions) }
+  it { should have_many(:subscribed_subphezes) }
+  it { should have_many(:transactions) }
+  it { should have_many(:oauth_applications) }
+end
